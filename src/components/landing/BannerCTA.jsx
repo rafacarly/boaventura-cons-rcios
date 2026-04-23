0d382254479192ca51cbb086fbc5ee2aec2ad9a1
Curiosidades pop <popcuriosidades80@gmail.com>
2026-04-23 12:10:06 +0000

    File changes

--- src/components/landing/BannerCTA.jsx
+++ src/components/landing/BannerCTA.jsx
@@ -99,10 +99,10 @@ export default function BannerCTA() {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 h-full max-w-7xl mx-auto px-6 lg:px-12 flex flex-col lg:flex-row items-center justify-between gap-2 pt-16 pb-8 lg:py-0">
+      <div className="relative z-10 h-full max-w-7xl mx-auto px-6 lg:px-12 flex flex-col lg:flex-row items-center justify-between gap-2 lg:py-0" style={{paddingTop: "10%", paddingBottom: "5%"}}>
 
         {/* Left - Text */}
-        <div className="flex-1 w-full mt-6 lg:mt-0">
+        <div className="flex-1 w-full lg:mt-0">
           <AnimatePresence mode="wait">
             <motion.div
               key={current}
